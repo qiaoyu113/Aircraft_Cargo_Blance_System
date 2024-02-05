e0d49e1b09e4c3ae6c0db71470d777a4908f3910
乔宇 <qy0803@yeah.net>
2024-02-05 17:16:36 +0000

    Updated the monitoring status page

--- frontend/src/views/index.vue
+++ frontend/src/views/index.vue
@@ -4,30 +4,17 @@
 
       <dv-loading v-if="loading">Loading...</dv-loading>
       <div v-else class="host-body">
-          <!-- <dv-decoration-10 style="width:33.3%;height:5px;" />
-          <div class="d-flex jc-center">
-            <dv-decoration-8 :color="['#568aea', '#000000']" style="width:200px;height:50px;" />
-            <div class="title">
-              <span class="title-text">Aircraft Cargo Blance System</span>
-              <dv-decoration-6
-                class="title-bototm"
-                :reverse="true"
-                :color="['#50e3c2', '#67a1e5']"
-                style="width:250px;height:8px;"
-              />
-            </div>
-            <dv-decoration-8
-              :reverse="true"
-              :color="['#568aea', '#000000']"
-              style="width:200px;height:50px;"
-            />
-          </div>
-          <dv-decoration-10 style="width:33.3%;height:5px;" /> -->
           <div class="nav">
             <div class="nav-text">
               <span class="wel">Welcome to </span>
               <span> Aircraft Cargo Blance System</span>
             </div>
+            <dv-decoration-2 :reverse="false" style="width: 300px;
+              height: 5px;
+              position: relative;
+              top: 160px;
+              left: 40px;
+              z-index: 1000;" />
             <div class="nav-img"></div>
             <div class="nav-shadow"></div>
           </div>
@@ -35,19 +22,14 @@
         <div class="body-box">
           <!-- 第三行数据 -->
           <div class="content-box">
-            <div>
-              <!-- <centreLeft1 /> -->
-            </div>
-            <!-- 中间 -->
+            <!-- 左侧 -->
             <div>
               <!-- <dv-border-box-12> -->
               <bottomLeft />
               <!-- </dv-border-box-12> -->
             </div>
             <!-- 右侧 -->
-            <!-- <div>
-              <center />
-            </div> -->
+            <status></status>
           </div>
         </div>
       </div>
@@ -58,6 +40,7 @@
 <script>
 // import center from "./center";
 import bottomLeft from "./bottomLeft";
+import status from "./status";
 // import centreLeft1 from "./centreLeft1";
 export default {
   data() {
@@ -66,9 +49,8 @@ export default {
     };
   },
   components: {
-    // center,
     bottomLeft,
-    // centreLeft1
+    status
   },
   mounted() {
     this.cancelLoading();
