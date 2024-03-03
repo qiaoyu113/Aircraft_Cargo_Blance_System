0fa67de2e8aa45c809bb2c5358be082abebb6e53
乔宇 <qy0803@yeah.net>
2024-03-03 14:20:32 +0000

    test

--- backend/app/conveyor_status/conveyor_status.cpp
+++ backend/app/conveyor_status/conveyor_status.cpp
@@ -1,9 +1,19 @@
 // conveyor_status.cpp
 #include "conveyor_status.hpp"
 
-ConveyorStatus::ConveyorStatus(MessageSender& sender) : messageSender(sender) {}
+// ConveyorStatus::ConveyorStatus(MessageSender& sender) : messageSender(sender) {}
+ConveyorStatus::ConveyorStatus() : messageSender(nullptr) {}
+
+void ConveyorStatus::setMessageSender(MessageSender& sender) {
+    messageSender = &sender;
+}
 
 void ConveyorStatus::changeConveyorStatus(int parameter) { // 添加 void 返回类型
+    if (messageSender == nullptr) {
+        // 处理未初始化 MessageSender 的情况
+        return;
+    }
+    
     int newCounter = parameter;
 
     // 只有当newCounter的值与lastCounter不同时，才执行发送逻辑
