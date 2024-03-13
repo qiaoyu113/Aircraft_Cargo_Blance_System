c3bfffef1573eff4cde539f512c948e839825ffa
joey <527324363@qq.com>
2024-03-13 14:40:36 +0000

    motor_driver_version1

--- backend/app/motor_driver/motor_driver.cpp
+++ backend/app/motor_driver/motor_driver.cpp
@@ -3,15 +3,21 @@
 #include <unistd.h>
 
 // 定义STEP和DIR引脚
-const int stepPin = 17; // 使用BCM引脚编号，根据实际连接修改
-const int dirPin = 27;  // 使用BCM引脚编号，根据实际连接修改
+const int stepPin = 19; // 使用BCM引脚编号，根据实际连接修改
+const int dirPin = 17;  // 使用BCM引脚编号，根据实际连接修改
 
-void stepMotor(int steps, int delayUs) {
+// 定义方向常量
+const int DIR_FORWARD = PI_HIGH; // 或者任何代表正向的值
+const int DIR_BACKWARD = PI_LOW; // 或者任何代表反向的值
+
+void stepMotor(int steps, int delayUs, int direction) {
+    gpioWrite(dirPin, direction); // 设置电机方向
     for (int i = 0; i < steps; i++) {
         gpioWrite(stepPin, 1);
         usleep(delayUs);
         gpioWrite(stepPin, 0);
         usleep(delayUs);
+        //printf("doing so");
     }
 }
 
@@ -25,13 +31,48 @@ int main() {
     gpioSetMode(stepPin, PI_OUTPUT);
     gpioSetMode(dirPin, PI_OUTPUT);
 
-    // 设置电机方向
-    gpioWrite(dirPin, PI_HIGH); // 设置为PI_HIGH或PI_LOW以改变方向
-
     // 旋转步进电机
-    stepMotor(200, 1000); // 旋转200步，每步间隔1000微秒
+    stepMotor(800, 500, DIR_FORWARD); // 正向旋转200步
+    stepMotor(800, 500, DIR_BACKWARD); // 反向旋转200步
 
     gpioTerminate();
 
     return 0;
 }
+// #include <iostream>
+// #include <pigpio.h>
+// #include <unistd.h>
+
+// // 定义STEP和DIR引脚
+// const int stepPin = 19; // 使用BCM引脚编号，根据实际连接修改
+// const int dirPin = 17;  // 使用BCM引脚编号，根据实际连接修改
+
+// void stepMotor(int steps, int delayUs) {
+//     for (int i = 0; i < steps; i++) {
+//         gpioWrite(stepPin, 1);
+//         usleep(delayUs);
+//         gpioWrite(stepPin, 0);
+//         usleep(delayUs);
+//     }
+// }
+
+// int main() {
+//     if (gpioInitialise() < 0) {
+//         std::cerr << "pigpio初始化失败！" << std::endl;
+//         return 1;
+//     }
+
+//     // 设置GPIO引脚模式
+//     gpioSetMode(stepPin, PI_OUTPUT);
+//     gpioSetMode(dirPin, PI_OUTPUT);
+
+//     // 设置电机方向
+//     gpioWrite(dirPin, PI_HIGH); // 设置为PI_HIGH或PI_LOW以改变方向
+
+//     // 旋转步进电机
+//     stepMotor(800, 500); // 旋转200步，每步间隔1000微秒
+
+//     gpioTerminate();
+
+//     return 0;
+// }
